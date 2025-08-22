58c51b55e621e612ce24eb62c8a7cda047b93e6f
shokhruzisroilov <shokhruzisroilov@gmail.com>
2025-08-22 16:41:04 +0500

    testing

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,5 +1,9 @@
 const Services = () => {
-  return <div className="text-white border-2 border-white max-w-[1440px] mx-auto">Services</div>;
-};
+	return (
+		<div className='text-white border-2 border-white max-w-[1440px] mx-auto'>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, rem?
+		</div>
+	)
+}
 
-export default Services;
+export default Services
